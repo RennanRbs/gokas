592a3fe991ffd2a1f950f74673a3dcbc3b0773a8
rennanrbs <rennan.melo@lit.ifce.edu.br>
2020-10-28 00:38:09 -0300

    Initial Commit

---
+++ gokas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gokas
+//
+//  Created by Rennan Rebouças on 28/10/20.
+//  Copyright © 2020 Rennan Rebouças. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
