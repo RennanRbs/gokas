50b2881dd723ed5d64228855f1dac9ee95f80e20
rennanrbs <rennan.melo@lit.ifce.edu.br>
2020-10-28 02:56:51 -0300

    first commit

---
+++ gokas/Cells/CollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionViewCell.swift
+//  gokas
+//
+//  Created by Rennan Rebouças on 28/10/20.
+//  Copyright © 2020 Rennan Rebouças. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+}
--- gokas/ViewController.swift
+++ gokas/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
 
+    @IBOutlet weak var iconView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        setupCollection()
     }
+    
+    func setupCollection(){
+        collectionView?.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cellHeader")
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "id")
+        collectionView.backgroundColor = .clear
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if (indexPath.row == 1 || indexPath.row == 3){
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellHeader", for: indexPath) as! CollectionViewCell
+            return cell
+        }
+        else {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "id", for: indexPath)
+        cell.backgroundColor = .green
+            return cell
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if (indexPath.row == 1 || indexPath.row == 3) {return CGSize(width: collectionView.frame.width, height: 40)}
+        else { return CGSize(width: collectionView.frame.width, height: 200)}
+    }
+    
 
-
+    
 }
+
+
+
 
